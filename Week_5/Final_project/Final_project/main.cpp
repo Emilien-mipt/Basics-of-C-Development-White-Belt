9c6604fcb925002bffd5dfb40b915376e7a09815
Emin Tagiev <emin.tagiev@phystech.edu>
2019-01-20 22:42:16 +0300

    Made some changes in the tasks from the previous week

---
+++ Week_5/Final_project/Final_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final_project
+//
+//  Created by Emin Tagiev on 12/01/2019.
+//  Copyright © 2019 Emin Tagiev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
